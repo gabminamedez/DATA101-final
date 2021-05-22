492e072bf75a28a96842aeba2cc0c4f084597b8b
Lorenzo Mercado <ph.enzomercado@gmail.com>
2021-05-22 10:05:29 +0800

    Update homepage route in views.py to fail adding data when table exists

--- ligtas_ph/views.py
+++ ligtas_ph/views.py
@@ -10,9 +10,9 @@ def main():
     This route loads main.html 
     - Request for user to input from sentence fill.
     '''
-    DB_DATA.incident().to_sql(name='incident',if_exists='replace', con=db.engine, index=False)
-    DB_DATA.vehicle().to_sql(name='incident_vehicles',if_exists='replace', con=db.engine, index=False)
-    DB_DATA.tweet().to_sql(name='incident_tweet', con=db.engine,if_exists='replace', index=False)
+    DB_DATA.incident().to_sql(name='incident',if_exists='fail', con=db.engine, index=False)         #change if_exists to replace when resetting database to default
+    DB_DATA.vehicle().to_sql(name='incident_vehicles',if_exists='fail', con=db.engine, index=False) #change if_exists to replace when resetting database to default
+    DB_DATA.tweet().to_sql(name='incident_tweet', con=db.engine,if_exists='fail', index=False)      #change if_exists to replace when resetting database to default
     if request.method == 'POST':
         city = request.form.get('city')
         vehicle = request.form.get('vehicle')
