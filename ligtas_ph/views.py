5cb2b435ba49eda8273c810f997ec73ea1c8b123
gabminamedez <gabminamedez@gmail.com>
2021-05-25 10:56:23 +0800

    Configuring redirect on submit

--- ligtas_ph/views.py
+++ ligtas_ph/views.py
@@ -1,10 +1,32 @@
-from . import db,DB_DATA
-from flask import Blueprint, render_template, request, flash
+from . import db, DB_DATA
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 views = Blueprint('views', __name__)
 
+@views.route('/results', methods=['GET', 'POST'])
+def results():
+    '''
+    This route loads main.html 
+    - Request for user to input from sentence fill.
+    '''
+    DB_DATA.incident().to_sql(name='incident', if_exists='replace', con=db.engine, index=False)         #change if_exists to replace when resetting database to default
+    DB_DATA.vehicle().to_sql(name='incident_vehicles', if_exists='replace', con=db.engine, index=False) #change if_exists to replace when resetting database to default
+    DB_DATA.tweet().to_sql(name='incident_tweet', con=db.engine, if_exists='replace', index=False)      #change if_exists to replace when resetting database to default
+    if request.method == 'POST':
+        city = request.form.get('city')
+        vehicle = request.form.get('vehicle')
+
+        if city == '':
+            flash('City must be inputted to continue', category='error')
+        elif vehicle == '':
+            flash('Vehicle must be inputted to continue', category='error')
+
+        
+
+    return render_template('results.html')
+
 # Route will begin with initial sentence for data filtering
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 def main():
     '''
     This route loads main.html 
@@ -22,4 +44,28 @@ def main():
         elif vehicle == '':
             flash('Vehicle must be inputted to continue', category='error')
 
+        return redirect(url_for('results'))
+
     return render_template('main.html')
+
+# @views.route('/results', methods=['GET', 'POST'])
+# def results():
+#     '''
+#     This route loads main.html 
+#     - Request for user to input from sentence fill.
+#     '''
+#     DB_DATA.incident().to_sql(name='incident', if_exists='replace', con=db.engine, index=False)         #change if_exists to replace when resetting database to default
+#     DB_DATA.vehicle().to_sql(name='incident_vehicles', if_exists='replace', con=db.engine, index=False) #change if_exists to replace when resetting database to default
+#     DB_DATA.tweet().to_sql(name='incident_tweet', con=db.engine, if_exists='replace', index=False)      #change if_exists to replace when resetting database to default
+#     if request.method == 'POST':
+#         city = request.form.get('city')
+#         vehicle = request.form.get('vehicle')
+
+#         if city == '':
+#             flash('City must be inputted to continue', category='error')
+#         elif vehicle == '':
+#             flash('Vehicle must be inputted to continue', category='error')
+
+        
+
+#     return render_template('results.html')
